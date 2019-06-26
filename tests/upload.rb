292f47596851aa363c56f00c77bbb162230e7d2d
Muhammadreza Haghiri <haghiri75@gmail.com>
2019-06-26 21:33:22 +0430

    Some issues fixed

--- tests/upload.rb
+++ tests/upload.rb
@@ -10,7 +10,7 @@ post '/upload' do
     @filename = params[:file][:filename] 
     file = params[:file][:tempfile] 
 
-    File.open("./public/#{filename}", 'wb') do |f| 
+    File.open("./public/#{@filename}", 'wb') do |f| 
         f.write(file.read)
     end 
 
