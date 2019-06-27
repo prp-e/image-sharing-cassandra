585d496e6d9ce47126b35aeec10562e6aba9d2fb
Muhammadreza Haghiri <haghiri75@gmail.com>
2019-06-27 12:33:43 +0430

    Just some initiations

--- app.rb
+++ app.rb
@@ -2,3 +2,17 @@ require 'sinatra'
 require 'cassandra' 
 
 # This is the main app file, everything usable will be here. 
+
+cluster = Cassandra.cluster
+
+#This is just to make sure cluster is connected and works fine : 
+cluster.each_host do |host| 
+ puts "Host: #{host.ip}"
+ puts "ID: #{host.id}" 
+ puts "Datacenter: #{host.datacenter}" 
+ puts "Rack: #{host.rack}" 
+ puts ":----------------------------:" 
+ puts "The cluster is OK." 
+end 
+
+
