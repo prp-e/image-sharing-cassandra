6f7e1c30ad30ac8874d6f727e6f8ea44721061da
Muhammadreza Haghiri <haghiri75@gmail.com>
2019-06-27 12:47:20 +0430

    Just added some maskhare bazi

--- app.rb
+++ app.rb
@@ -6,13 +6,33 @@ require 'cassandra'
 cluster = Cassandra.cluster
 
 #This is just to make sure cluster is connected and works fine : 
+
+print "Starting application."
+sleep 0.25
+print "."
+sleep 0.25
+print "."
+sleep 0.25
+print "."
+sleep 0.25 
+puts "."
+sleep 1
 cluster.each_host do |host| 
  puts "Host: #{host.ip}"
+ sleep 0.5
  puts "ID: #{host.id}" 
+ sleep 0.5 
  puts "Datacenter: #{host.datacenter}" 
+ sleep 0.5 
  puts "Rack: #{host.rack}" 
+ sleep 0.5
  puts ":----------------------------:" 
+ sleep 0.5 
  puts "The cluster is OK." 
 end 
+
+sleep 2
+
+enable :sessions 
 
 
